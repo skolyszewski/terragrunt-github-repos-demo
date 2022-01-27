a3a9988ef543594e7525bc6fb012785ce99b49a6
Grzegorz Skolyszewski <grzegorz.skolyszewski@container-solutions.com>
2022-01-27 10:49:10 +0100

    Working terraform-live

--- terraform-live/main.tf
+++ terraform-live/main.tf
@@ -2,13 +2,14 @@
 
 locals {
     organization = "gskolysz"
+    # token is loaded from TF_VAR_token
 }
 
 module "repository-one" {
     source = "../modules/all-in-one"
 
     organization = local.organization
-    # token is loaded from TF_VAR_token
+    token = var.token
     repo_name = "tf-managed-all-in-one-repo"
     description = "Repository managed by plain terraform, uses all-in-one module"
     protected_branch_pattern = "main"
@@ -21,7 +22,7 @@ module "repository_two" {
     source = "../modules/repository"
 
     organization = local.organization
-    # token is loaded from TF_VAR_token
+    token = var.token
     # Q: WHAT IF I WANT TO USE DIFFERENT TOKEN FOR THIS REPO?
     # A: new variable is needed, or I need to split this into multiple terraform modules :(
     repo_name = "tf-managed-separate-repo"
@@ -32,7 +33,7 @@ module "repository_two_branch_protection" {
     source = "../modules/branch-protection"
 
     organization = local.organization
-    # token is loaded from TF_VAR_token
+    token = var.token
     repository_id = module.repository_two.repository_id # Uses outputs
     protected_branch_pattern = "main"
     enforce_admins = true
